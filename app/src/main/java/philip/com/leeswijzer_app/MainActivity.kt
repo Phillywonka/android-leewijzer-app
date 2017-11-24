70ea5d8ca1b024d2d28833630c6de00f82f7566d
Philip Wong <mail.philip.wong@gmail.com>
2017-11-24 19:24:34 +0100

    Init repository

---
+++ app/src/main/java/philip/com/leeswijzer_app/MainActivity.kt
@@ -0,0 +1,14 @@
+package philip.com.leeswijzer_app
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
